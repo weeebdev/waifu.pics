b7b22b12abe941dfbb6bc0baa3d4c350315e6b34
AlphaKennyBudy <mr.adil777@gmail.com>
2020-08-27 15:07:21 +0600

    DONE: Web server redirect is done

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch");
+
 const ENDPOINTS = require("./lib/endpoints.js");
+const { response } = require("express");
+
+const API_URL = "https://waifu.pics/api/";
 
 const app = express();
 
@@ -12,6 +17,46 @@ app.get("/", (req, res) => {
     message: "This is a simple server to get images from waifu.pics",
   });
 });
+
+app.get("/:endpoint", async (req, res) => {
+  if (ENDPOINTS.includes(req.params.endpoint.toLocaleLowerCase())) {
+    fetchImage(req.params.endpoint.toLocaleLowerCase())
+      .then((response) => {
+        res.redirect(response.url);
+      })
+      .catch((err) =>
+        res.status(500).json({
+          message: err.message,
+        })
+      );
+  } else if (req.params.endpoint.toLocaleLowerCase() === "random") {
+    let endpoint = ENDPOINTS[
+      Math.floor(Math.random() * ENDPOINTS.length)
+    ].toLocaleLowerCase();
+    fetchImage(endpoint)
+      .then((response) => res.redirect(response.url))
+      .catch((err) =>
+        res.status(500).json({
+          message: err.message,
+        })
+      );
+  } else {
+    res.status(400).json({
+      message: "Bad endpoint",
+    });
+  }
+});
+
+function fetchImage(endpoint) {
+  return fetch(`${API_URL}${endpoint}`).then(async (response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      let result = await response.json();
+      throw new Error(result.message);
+    }
+  });
+}
 
 const PORT = process.env.PORT || 8080;
 
